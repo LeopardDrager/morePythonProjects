ff78ef6d72f00f0e47479d8ff3e6a4a8667e3825
Maguire <136644201+LeopardDrager@users.noreply.github.com>
2023-11-27 08:20:27 -0600

    Update lastnamemario.py

--- lastnamemario.py
+++ lastnamemario.py
@@ -2,4 +2,4 @@
     
 height = int(input("Input the height you would like for the pyramid!"))
 for i in range (height):
-    print(' ' * (height - i - 1) + "*" * (i + 1) + ' ' + "*" * (i+1))
+    print(' ' * (height - i - 1) + "#" * (i + 1) + ' ' + "#" * (i+1))
